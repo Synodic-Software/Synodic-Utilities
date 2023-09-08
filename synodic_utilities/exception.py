bee33435cc02409ef97d83b0fd76b08c8f6ba831
Asher Norland <asher.norland@gmail.com>
2023-09-08 15:40:53 -0400

    Add Plugin Exception Type

--- synodic_utilities/exception.py
+++ synodic_utilities/exception.py
@@ -17,3 +17,21 @@ class ProcessError(Exception):
             str -- The underlying error
         """
         return self._error
+
+
+class PluginError(Exception):
+    """Raised when there is a plugin error"""
+
+    def __init__(self, error: str) -> None:
+        self._error = error
+
+        super().__init__(error)
+
+    @property
+    def error(self) -> str:
+        """Returns the underlying error
+
+        Returns:
+            str -- The underlying error
+        """
+        return self._error
